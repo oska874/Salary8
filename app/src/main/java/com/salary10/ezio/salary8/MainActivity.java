b3f9906175247ad291e7deefcb09ce0c16cbb227
ezio <ezio_zhang@outlook.com>
2017-09-06 16:42:33 +0800

    测试drawlayout 侧滑框

--- app/src/main/java/com/salary10/ezio/salary8/MainActivity.java
+++ app/src/main/java/com/salary10/ezio/salary8/MainActivity.java
@@ -3,11 +3,15 @@ package com.salary10.ezio.salary8;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -197,11 +201,11 @@ public class MainActivity extends AppCompatActivity {
         return 0;
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
